a9213369101a3811066aefc2e3ddc63bf0ea470f
Samir Alba <h97384742@gmail.com>
2022-11-16 21:36:21 -0400

    Funciones de motores

--- Robot_Balancin.ino
+++ Robot_Balancin.ino
@@ -1,4 +1,4 @@
-
+#include <PID_v1.h>
 #include <basicMPU6050.h> 
 #define x_paso 2    // Define el Pin de STEP para Motor de eje X
 #define x_dire 5    // Define el Pin de DIR  para Motor de eje X
@@ -10,15 +10,20 @@
 #define z_dire 7
 #define z_habi 8
 
+double Setpoint, Input, Output;
+double Kp=2, Ki=5, Kd=1;
+
 int retardo = 3000;   // Menor numero el giro es mas rapido
 int tiempo = 100;   // durante cuanto timpo da el giro el motor (vueltas)
 basicMPU6050<> imu;
 
-
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup() {
   imu.setup();
-
+  //Encendemos el PID
+  myPID.SetMode(AUTOMATIC);
+  
   // Initial calibration of gyro
   imu.setBias();
   Serial.begin(38400);
@@ -29,29 +34,32 @@ pinMode(z_paso, OUTPUT); pinMode(z_dire, OUTPUT); pinMode(z_habi, OUTPUT);
 
 void loop() {
   imu.updateBias();
-  giro(y_paso,y_dire,y_habi);
-  giro(z_paso,z_dire,z_habi);
+  myPID.Compute();
+  girarRuedaDerecha(100,true);
+  girarRuedaIzquierda(100,true);
   mostrarDatosMPU();
 }
-
-void giro(int paso_,int dire_,int habi_) {
-   digitalWrite(habi_, LOW);  // Habilita el Driver
-   digitalWrite(dire_, LOW);   // direccion de giro 1
-   for(int i=0;i<tiempo;i++){  // da  pasos por un tiempo  
-    digitalWrite(paso_, HIGH);      
+void girarRuedaDerecha(int pasos,bool direccion){
+  digitalWrite(y_habi, LOW);  // Habilita el Driver
+  digitalWrite(y_dire, direccion);   // direccion de giro 1
+   for(int i=0;i<pasos;i++){  // da  pasos por un tiempo  
+    digitalWrite(y_paso, HIGH);      
     delayMicroseconds(retardo);          
-    digitalWrite(paso_, LOW);       
+    digitalWrite(y_paso, LOW);       
     delayMicroseconds(retardo); 
    }
-  digitalWrite(dire_, HIGH);   // direccion de giro 2
-  for(int i=0;i<tiempo;i++){   // da  pasos por un tiempo  
-    digitalWrite(paso_, HIGH);      
+   digitalWrite(y_habi, HIGH);   // quita la habilitacion del Driver
+}
+void girarRuedaIzquierda(int pasos,bool direccion){
+  digitalWrite(z_habi, LOW);  // Habilita el Driver
+  digitalWrite(z_dire, direccion);   // direccion de giro 1
+   for(int i=0;i<pasos;i++){  // da  pasos por un tiempo  
+    digitalWrite(z_paso, HIGH);      
     delayMicroseconds(retardo);          
-    digitalWrite(paso_, LOW);       
-    delayMicroseconds(retardo);  
-  }
-  digitalWrite(habi_, HIGH);   // quita la habilitacion del Driver
-
+    digitalWrite(z_paso, LOW);       
+    delayMicroseconds(retardo); 
+   }
+   digitalWrite(z_habi, HIGH);   // quita la habilitacion del Driver
 }
 void mostrarDatosMPU(){
     //-- Scaled and calibrated output:
