57806f90a656ca88caa76c08a9d72c374ca3253e
ZeroHour01 <h94384742@gmail.com>
2022-11-22 02:52:59 -0400

    Se implemento correctamente el calculo del angulo del MPU, tambien se descubrio que el la libreria del mpu tiene un buffer de datos por lo que no refleja correctamente lo que sucede en el momento

--- Robot_Balancin.ino
+++ Robot_Balancin.ino
@@ -9,57 +9,133 @@
 #define z_paso 4
 #define z_dire 7
 #define z_habi 8
+bool flag=false;
 
-double Setpoint, Input, Output;
-double Kp=2, Ki=5, Kd=1;
-
+int posicionesPromedio=4;
+int contadorPromedio=0;
 int retardo = 3000;   // Menor numero el giro es mas rapido
 int tiempo = 100;   // durante cuanto timpo da el giro el motor (vueltas)
-basicMPU6050<> imu;
 
+double y1Y;
+double y2Y;
+double integralY=0;
+double Setpoint=90, Input, Output;
+double Kp=20, Ki=0, Kd=0;
+double promedio=0;
+double degresY;
+double umbral=0.01;
+unsigned long timer=0;
+basicMPU6050<> imu;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup() {
+  
   imu.setup();
   //Encendemos el PID
   myPID.SetMode(AUTOMATIC);
-  
+  myPID.SetOutputLimits(-200,200);
   // Initial calibration of gyro
   imu.setBias();
   Serial.begin(38400);
-    
-pinMode(y_paso, OUTPUT); pinMode(y_dire, OUTPUT); pinMode(y_habi, OUTPUT);
-pinMode(z_paso, OUTPUT); pinMode(z_dire, OUTPUT); pinMode(z_habi, OUTPUT); 
+  pinMode(y_paso, OUTPUT); pinMode(y_dire, OUTPUT); pinMode(y_habi, OUTPUT);
+  pinMode(z_paso, OUTPUT); pinMode(z_dire, OUTPUT); pinMode(z_habi, OUTPUT); 
+ 
 }    
 
 void loop() {
   imu.updateBias();
-  myPID.Compute();
-  girarRuedaDerecha(100,true);
-  girarRuedaIzquierda(100,true);
-  mostrarDatosMPU();
+  promedio+=imu.gy();
+  contadorPromedio++;
+//  if(contadorPromedio==posicionesPromedio){
+//    contadorPromedio=0;
+//    promedio=promedio/posicionesPromedio;
+//  }
+
+  if(millis()-timer>100){
+    if(flag==true){
+        y1Y=promedio/contadorPromedio;
+        contadorPromedio=0;
+        promedio=0; 
+        flag=false;
+    }else{
+        if(millis()-timer>200){
+          timer=millis();
+          flag=true;
+          
+          y2Y=promedio/contadorPromedio;
+          if(abs(y1Y)>umbral){
+          double areaY=0.1*((y2Y+y1Y)/2);
+          integralY+=areaY;
+          }
+          degresY=(integralY*360)/(2*PI);
+          degresY+=90;
+          Input=degresY;
+          myPID.Compute();
+          mostrarDatosPID();
+          estimarDesplazamiento();
+        }
+    }
+  }
+
+  
+  //
+  //mostrarDatosMPU();
 }
-void girarRuedaDerecha(int pasos,bool direccion){
+void adelante(int pasos,int velocidad){
   digitalWrite(y_habi, LOW);  // Habilita el Driver
-  digitalWrite(y_dire, direccion);   // direccion de giro 1
-   for(int i=0;i<pasos;i++){  // da  pasos por un tiempo  
-    digitalWrite(y_paso, HIGH);      
-    delayMicroseconds(retardo);          
-    digitalWrite(y_paso, LOW);       
-    delayMicroseconds(retardo); 
-   }
-   digitalWrite(y_habi, HIGH);   // quita la habilitacion del Driver
-}
-void girarRuedaIzquierda(int pasos,bool direccion){
+  digitalWrite(y_dire, LOW);   // direccion de giro 1
   digitalWrite(z_habi, LOW);  // Habilita el Driver
-  digitalWrite(z_dire, direccion);   // direccion de giro 1
-   for(int i=0;i<pasos;i++){  // da  pasos por un tiempo  
-    digitalWrite(z_paso, HIGH);      
-    delayMicroseconds(retardo);          
-    digitalWrite(z_paso, LOW);       
-    delayMicroseconds(retardo); 
-   }
-   digitalWrite(z_habi, HIGH);   // quita la habilitacion del Driver
+  digitalWrite(z_dire, LOW);   // direccion de giro 1
+  for(int i=0;i<pasos;i++){  // da  pasos por un tiempo  
+    digitalWrite(y_paso, HIGH); 
+    digitalWrite(z_paso, HIGH);     
+    delayMicroseconds(velocidad);          
+    digitalWrite(y_paso, LOW);
+    digitalWrite(z_paso, LOW);        
+    delayMicroseconds(velocidad); 
+  }
+  digitalWrite(z_habi, HIGH);
+  digitalWrite(y_habi, HIGH);
+}
+void atras(int pasos,int velocidad){
+
+  
+  digitalWrite(y_habi, LOW);  // Habilita el Driver
+  digitalWrite(y_dire, HIGH);   // direccion de giro 1
+  digitalWrite(z_habi, LOW);  // Habilita el Driver
+  digitalWrite(z_dire, HIGH);   // direccion de giro 1
+  for(int i=0;i<pasos;i++){  // da  pasos por un tiempo  
+    digitalWrite(y_paso, HIGH); 
+    digitalWrite(z_paso, HIGH);     
+    delayMicroseconds(velocidad);          
+    digitalWrite(y_paso, LOW);
+    digitalWrite(z_paso, LOW);        
+    delayMicroseconds(velocidad); 
+  }
+  digitalWrite(z_habi, HIGH);
+  digitalWrite(y_habi, HIGH);
+}
+void estimarDesplazamiento(){
+    //Se requiere 200 pasos para dar una vuelta completa al motor
+    if(Output>0){
+       
+        adelante(int(Output),1300);
+        
+    }
+    if(Output<0){
+      
+      atras(int(abs(Output)),1300);  
+    }
+    
+}
+void mostrarDatosPID(){
+  
+  Serial.print("DegresY:"); Serial.print(degresY); Serial.print(", ");
+  Serial.print("Setpoint:"); Serial.print(Setpoint); Serial.print(", ");
+  Serial.print("Input:"); Serial.print(Input); Serial.print(", ");
+  Serial.print("Output:"); Serial.print(Output); Serial.print(", ");
+ Serial.println();
+  
 }
 void mostrarDatosMPU(){
     //-- Scaled and calibrated output:
